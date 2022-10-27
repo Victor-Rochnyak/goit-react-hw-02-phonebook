21ad9a66c9e94e03d6fa5ab80da56ba4524d315f
Victor-Rochnyak <victor.rochnyak@gmail.com>
2022-10-27 19:55:35 +0300

    /

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, number, value } = event.currentTarget;
+    this.setState({ [name]: value });
+    this.setState({ [number]: value });
+  };
+  render() {
+    return (
+      <>
+        <h1>PhoneBook</h1>
+        <form>
+          <label htmlFor="">
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title={this.state.contacts.name}
+              required
+            />
+          </label>
+          <label htmlFor="">
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleChange}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Click</button>
+        </form>
+        <h1>Contacts</h1>
+        <ul>
+          <li></li>
+        </ul>
+      </>
+    );
+  }
+}
+export default App;
+{
+  /* <PhoneBook />; */
+}
