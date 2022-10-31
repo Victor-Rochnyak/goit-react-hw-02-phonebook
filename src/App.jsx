be0357612c30058b34c3096b205f6072bb171a75
Victor-Rochnyak <103627445+Victor-Rochnyak@users.noreply.github.com>
2022-10-31 22:17:01 +0200

    /

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,18 @@ class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
+    // filter: '',
   };
-// добавдяємо контакт та номер телефону по сабміту
-  addContact= ({name,number})=>{
-    const contact ={
-      id:nanoid(),
+  // добавдяємо контакт та номер телефону по сабміту
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
       name,
       number,
     };
-    this.setState(({contacts})=>({
-      contacts:[contact, ...contacts],
-    }))
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
   };
 
   // Метод видалення контакту по ID //Працюємо лишe з map(), reduce(), filter()
@@ -31,16 +32,18 @@ class App extends Component {
     }));
   };
 
-  
-
   render() {
-    const { contacts } = this.state;
+    const { contacts} = this.state;
+
     return (
       <>
         <h1>PhoneBook</h1>
         <FormBook onSubmit={this.addContact} />
         <h1>Contacts</h1>
-        <ContactsList contacts={contacts} onDeleteContact={this.deleteContact} />
+        <ContactsList
+          contacts={contacts}
+          onDeleteContact={this.deleteContact}
+        />
       </>
     );
   }
